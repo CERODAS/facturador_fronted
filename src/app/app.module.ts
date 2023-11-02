7f6d0a772105a1a81409ca615a30eae6c3321165
Carlos Rodas <crodasv2@miumg.edu.gt>
2023-11-01 22:38:39 -0600

    Se crea componente, servicio y modulo para consumir microservicio de cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TablaComponent } from './componentes/tabla/tabla.component';
+import { ClienteComponent } from './componentes/cliente/cliente.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TablaComponent
+    ClienteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/cliente-model.ts
@@ -0,0 +1,13 @@
+export class clienteModel{
+    id_cliente: number = 2;
+    nombre: string = '';
+    dpi: string = '';
+    telefono: string = '';
+    direccion: string = '';
+    nit: string = '';
+    fecha_creacion: string = '';
+    usuario_creacion: string = '';
+    fecha_mod: string = '';
+    usuario_mod: string = '';
+    estado: boolean = false
+}
